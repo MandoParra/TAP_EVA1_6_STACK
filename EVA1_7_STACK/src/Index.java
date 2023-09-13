a3f3df1a81e523a61007be1c67f5810c4c6b191b
MandoParra <111606634+MandoParra@users.noreply.github.com>
2023-09-12 18:52:02 -0600

    Add files via upload

---
+++ EVA1_7_STACK/src/Index.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Manuel Fernando Parra Gonzalez 12/09/2023
+ */
+public class Index {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("INICIA MAIN");
+        A();
+        System.out.println("FIN MAIN");
+    }
+    public static void A(){
+        System.out.println("INICIA A");
+        B();
+        System.out.println("FIN A");
+    }
+    public static void B(){
+        System.out.println("INICIA B");
+        System.out.println("FIN B");
+    }
+    
+}
